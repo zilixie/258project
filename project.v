d477047ea734d23f400d2a2aa1a1d55ee4821879
HUADONG XING <huadong.xing@mail.utoronto.ca>
2016-11-16 21:10:56 -0500

    add comments

--- project.v
+++ project.v
@@ -2,7 +2,16 @@ module project
 	(
 		CLOCK_50,						//	On Board 50 MHz
 		// Your inputs and outputs here
-                KEY,
+      KEY,
+		// The ports below are for the VGA output.  Do not change.
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
 	);
 
 	input		CLOCK_50;				//	50 MHz
@@ -10,10 +19,18 @@ module project
 
 	// Declare your inputs and outputs here
 	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
 	 
 
 	wire reset_n;
-	assign reset_n = KEY[3];
+	assign reset_n = 1'b0;
 	
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
 	wire [2:0] colour;
@@ -21,33 +38,62 @@ module project
 	wire [7:0] y;
 	wire writeEn;
 
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(reset_n),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+			
+	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+	// for the VGA controller, in addition to any other functionality your design may require.
 
-	wire [1:0]option;
-	wire counter_enable;
-        wire [7:0] x_pos;
+
+	wire [1:0]op;
+	wire counter_enable, load;
+   wire [7:0] x_pos;
 
 
 	datapath d0(
 					.x_in(x_pos),
-					.y_in(8'd60),
-                                        .op(option),
+					.y_in(8'd110),
+               .op_in(op),
 					.clk(CLOCK_50),
 					.reset_n(reset_n),
 					.enable(counter_enable),
 					.x_out(x),
 					.y_out(y),
-					.color_out(colour)
+					.color_out(colour),
+					.load(load)
 					);
 
-    // Instansiate FSM control
+   // Instansiate FSM control
 	control c0(
 				.clk(CLOCK_50),
 				.button(KEY[3:0]),
 				.reset_n(reset_n),
 				.plot(writeEn),
-				.op(option),
-                                .x(x_pos),
-                                .counter_enable(counter_enable)
+				.op(op),
+            .x(x_pos),
+            .counter_enable(counter_enable),
+				.load(load)
 				);
 
 
@@ -55,33 +101,39 @@ module project
 endmodule
 
 
-module control(clk, button, reset_n, op, x, counter_enable, plot);
-        input clk, reset_n;
-        input [3:0] button;
-        output reg [1:0] op;
-        output reg [7:0] x = 8'd80;
-        output reg plot, counter_enable;
+module control(
+        input clk, reset_n,
+        input [2:0] button,
+        output reg [1:0] op,
+        output reg [7:0] x = 8'd80,
+        output reg plot, counter_enable, load
+		  );
 
 	reg [1:0] current_state, next_state;
-        reg [4:0] draw_count, erase_count;
-        reg [20:0] wait_count;
-        reg enable_draw, enable_wait, enable_erase;
+	reg enable_fire = 1'b0;
+   reg [4:0] draw_count, erase_count;
+   reg [20:0] wait_count;
+	reg [24:0] fire_count;
+   reg enable_draw, enable_wait;
 
-        wire draw_done, erase_done, wait_go;
-        localparam DRAW  = 2'b00,
-                   ERASE = 2'b01,
-                   WAIT  = 2'b10;
-
-        always @(*)
-        begin
-
+   wire done, wait_go, stop_fire;
+   localparam DRAW   = 2'b00,
+              ERASE  = 2'b01,
+              WAIT   = 2'b10,
+				  S_LOAD = 2'b11;
+				  
+	
+	// State table
+	always @(*)
+   begin
 		case (current_state)
-		DRAW: next_state = draw_done ? WAIT : DRAW;
-		WAIT: next_state = wait_go ? ERASE : WAIT;
-		ERASE: next_state = erase_done ? DRAW : ERASE;
-		default: next_state = DRAW;
-                endcase
-        end
+			S_LOAD: next_state = DRAW;
+			DRAW: next_state = done ? WAIT : DRAW;
+			WAIT: next_state = wait_go ? ERASE : WAIT;
+			ERASE: next_state = done ? S_LOAD : ERASE;
+			default: next_state = S_LOAD;
+      endcase
+	end
 
 	always @(*)
 	begin
@@ -89,140 +141,189 @@ module control(clk, button, reset_n, op, x, counter_enable, plot);
 		counter_enable = 1'b0;
 		enable_draw = 1'b0;
 		enable_wait = 1'b0;
-                enable_erase = 1'b0;
+		op = 2'b00;
+		load = 1'b0;
 		case (current_state)
-			DRAW: 
-				begin
-		                        plot = 1'b1;
-		                        counter_enable = 1'b1;
-                                        op = 2'b00;
-                                        enable_draw = 1'b1;
-					enable_wait = 1'b0;
-                                        enable_erase = 1'b0;
-				end
-			WAIT:
-				begin
-		                        plot = 1'b1;
-		                        counter_enable = 1'b0;
-                                        enable_draw = 1'b0;
-					enable_wait = 1'b1;
-                                        enable_erase = 1'b0;
-				end
-			ERASE:
-				begin
-		                        plot = 1'b1;
-		                        counter_enable = 1'b1;
-                                        op = 2'b01;
-                                        enable_draw = 1'b0;
-					enable_wait = 1'b0;
-                                        enable_erase = 1'b1;
-				end
-                        default:
-				begin
-		                plot = 1'b1;
-		                counter_enable = 1'b0;
+		
+		S_LOAD:begin
+			load = 1'b1; // Load x, y to datapath.
+		end
+		DRAW: begin
+         if (enable_fire == 1'b0)
+         begin
+		      plot = 1'b1;
+				counter_enable = 1'b1;
+		      op = 2'b00;
+		      enable_draw = 1'b1;
+				enable_wait = 1'b0;
+         end
+         else if (enable_fire == 1'b1) // If enable_fire, then set op to 2.
+         begin
+		      plot = 1'b1;
+				counter_enable = 1'b1;
+		      op = 2'b10;
+		      enable_draw = 1'b1;
+				enable_wait = 1'b0;
+           end
+		end
+		WAIT:begin
+			 plot = 1'b1;
+			 counter_enable = 1'b0;
+			 enable_draw = 1'b0;
+			 enable_wait = 1'b1;
+		end
+		ERASE:begin
+		       plot = 1'b1;
+		       counter_enable = 1'b1;
+             op = 2'b01;
+             enable_draw = 1'b1;
+				 enable_wait = 1'b0;
+		end
+      default:begin
+				plot = 1'b1;
+				counter_enable = 1'b0;
 				enable_draw = 1'b0;
 				enable_wait = 1'b0;
-				enable_erase = 1'b0;
-				end
+				op = 2'b00;
+		end
 		endcase
 		
 	end
 
-        always @(posedge clk)
+	// Draw counter, generate done signal.
+   always @(posedge clk)
 	begin
-		if (reset_n == 1'b0)
-                	draw_count <= 5'b00000;
-	   	else if (enable_draw == 1'b1)
-			begin
-			if (draw_count == 5'b11000)
-                                begin
-				draw_count <= 5'b00000;
-                                end
-			else
-				draw_count = draw_count + 1'b1;
-			end
+	if (reset_n == 1'b0)
+		draw_count <= 5'd0;
+	else if (enable_draw == 1'b1)
+		begin
+		if (draw_count == 5'd25)
+			draw_count <= 5'd0;
+		else
+			draw_count = draw_count + 1'b1;
+		end
 	end
-        assign draw_done = (draw_count == 5'b11000) ? 1'b1 : 1'b0;
+   
+	assign done = (draw_count == 5'd25) ? 1'b1 : 1'b0;
 
-
-        always @(posedge clk)
+	// Go counter, generate go signal
+   always @(posedge clk)
 	begin
 		if (reset_n == 1'b0)
-                	wait_count <= 21'b0;
-	   	else if (enable_wait == 1'b1)
+			wait_count <= 21'b0;
+	   else if (enable_wait == 1'b1)
 			begin
-			if (wait_count == 21'd25)
-                                begin
+			if (wait_count == 21'd1666666)
 				wait_count <= 21'b0;
-                                end
 			else
 				wait_count = wait_count + 1'b1;
 			end
 	end
-        assign wait_go = (wait_count == 21'd25) ? 1'b1 : 1'b0;
-
-
-        always @(posedge clk)
+   
+	assign wait_go = (wait_count == 21'd1666666) ? 1'b1 : 1'b0;
+	
+	// State register.
+	always @(posedge clk)
 	begin
-	if (reset_n == 1'b0)
-                erase_count <= 5'b00000;
-	else if (enable_erase == 1'b1)
+	if(!reset_n)
+		current_state <= S_LOAD;
+	else
+		current_state <= next_state;
+	end
+
+	//  Left and right button counter, only react press every 0.1 second.
+	reg [28:0] read_btn_c;
+	wire read_btn_en;
+		
+	always @(posedge clk)
+	begin
+	if (!reset_n)
+		read_btn_c <= 28'd0;
+	else
 		begin
-		if (erase_count == 5'b11000)
-                        begin
-			erase_count <= 5'b00000;
-                        end
+		if (read_btn_c == 28'd0)
+			read_btn_c <= 28'd5000000;
 		else
-			erase_count = erase_count + 1'b1;
+			read_btn_c <= read_btn_c - 1'b1;
 		end
 	end
-        assign erase_done = (erase_count == 5'b11000) ? 1'b1 : 1'b0;
-
-
-
-
-	always@(posedge clk)
+	
+	assign read_btn_en = (read_btn_c == 28'd0) ? 1'b1 : 1'b0;
+	
+	// X position register.
+   always @(posedge clk)
+   begin
+   if(!reset_n)
+		x <= 8'd80;
+	else if (~button[0] & read_btn_en)
+		x <= x + 1'b1;
+	else if (~button[1] & read_btn_en)
+		x <= x - 1'b1;
+	end
+	
+	// Fire button counter, only react fire button 1 time a second.
+	reg [28:0] read_fire_c;
+	wire read_fire_en;
+		
+	always @(posedge clk)
+	begin
+	if (!reset_n)
+		read_fire_c <= 28'd0;
+	else
 		begin
-			if(!reset_n)
-				current_state <= DRAW;
-			else
-				current_state <= next_state;
+		if (read_fire_c == 28'd0)
+			read_fire_c <= 28'd5000000;
+		else
+			read_fire_c <= read_fire_c - 1'b1;
 		end
-
-
-        always@(negedge button[0], negedge button[1])
-                begin
-                        if(!reset_n)
-				x <= 8'd0;
-			else if (~button[0])
-				x <= x + 1'b1;
-			else if (~button[1])
-				x <= x - 1'b1;
+	end
+	
+	assign read_fire_en = (read_fire_c == 28'd0) ? 1'b1 : 1'b0;
+	
+	// Fire register.
+   always @(posedge clk)
+   begin
+   if(!reset_n)
+		enable_fire <= 1'b0;
+	else if (~button[2] & read_fire_en)
+		enable_fire <= 1'b1;
+	else if (stop_fire == 1'b1)
+	   enable_fire <= 1'b0;
+	end
+	
+	// Every fire remain for 0.25 sec.
+	always @(posedge clk)
+	begin
+	if (reset_n == 1'b0)
+      fire_count <= 25'd0;
+	else if (enable_fire == 1'b1)
+		begin
+		if (fire_count == 25'd1249999)
+			fire_count <= 25'd0;
+		else
+			fire_count = fire_count + 1'b1;
 		end
+	end
+	
+	assign stop_fire = (fire_count == 25'd1249999) ? 1'b1 : 1'b0;
 
 endmodule
 
 
-
-
-
-
-
-
-
-
+// Datapath
 
 module datapath(
 					input [7:0] x_in, y_in,
-					input [1:0] op,
-					input clk, reset_n, enable,
+					input [1:0] op_in,
+					input clk, reset_n, enable, load,
 					output [7:0] x_out, y_out,
 					output reg [2:0] color_out
 					);
 					
 	reg [2:0] x_count, y_count;
 	wire en_y_count;
+	reg [7:0] x, y;
+	//reg [1:0] op;
 	
 	localparam  draw       = 2'b00,
 		    erase      = 2'b01,
@@ -231,6 +332,22 @@ module datapath(
 		    black      = 3'b000,
 		    white      = 3'b111,
 		    red        = 3'b100;
+	
+	always @(posedge clk)
+	begin
+		if (!reset_n)
+		begin
+			x <= 8'd80;
+			y <= 8'd110;
+			//op <= 2'd0;
+		end
+		else if (load == 1'b1)
+		begin
+			x <= x_in;
+			y <= y_in;
+			//op <= op_in;
+		end
+	end
 	
 	// X counter
 	always @(posedge clk)
@@ -252,7 +369,7 @@ module datapath(
 		end
 	end
 	
-	assign en_y_count = (x_count == 3'b000) ? 1 : 0;
+	assign en_y_count = (x_count == 3'b000) ? 1'b1 : 1'b0;
 	
 	// Y counter
 	always @(posedge clk)
@@ -268,14 +385,14 @@ module datapath(
 		end
 	end
 	
-	assign x_out = x_in + x_count;
-	assign y_out = y_in + y_count;
+	assign x_out = x + x_count;
+	assign y_out = y + y_count;
 	
 	// Color handle
 	always @(*)
 	begin
 	color_out = black;
-	case (op)
+	case (op_in)
 		draw: begin
 			if (y_count == 3'b001 & x_count == 3'b010)
 				color_out = white;
@@ -315,6 +432,8 @@ module datapath(
 					color_out = white;
 				end
 			end
+			
+			default: color_out = black;
 			
 	endcase
 	end
